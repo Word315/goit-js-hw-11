d9952aec64506d5bb6f004861ca83ef530ca87bc
Word315 <boichuk315@gmail.com>
2025-03-28 13:56:46 +0000

    Deploying to gh-pages from @ Word315/goit-js-hw-11@2d9d82c3868021978f4c2f0e7063d2347fe7360d 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-njWUcVeN.js";document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.elements.delay.value),o=t.target.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{o==="fulfilled"?e(s):m(s)},s)}).then(e=>{i.success({message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=2-snackbar.js.map
